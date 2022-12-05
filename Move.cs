aa5fc47d298e1d807f6e1f30d6145fbcf6c0cf29
hard.tender.blade <92087058+DenisMatsenko@users.noreply.github.com>
2022-12-05 10:38:17 +0100

    Upload player move script

---
+++ Move.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private Camera Camera;
+    [SerializeField] private bool InverseControl;
+    [SerializeField] private float Speed;
+    [SerializeField] private float maxInputValue;
+    [SerializeField] private float gravityForce;
+    
+    private Vector3 velocity;
+    private Rigidbody rb;
+    private float horizontalInput;
+    private float verticalInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        //Max speed by gravity
+        rb.maxAngularVelocity = 100;
+    }
+
+    void Update()
+    {
+        //Inputs with inverse
+        horizontalInput = InverseControl ? Input.GetAxis("Mouse X") *-1 : Input.GetAxis("Mouse X");
+        verticalInput = InverseControl ? Input.GetAxis("Mouse Y") * -1 : Input.GetAxis("Mouse Y");
+
+        //Input limit control
+        if (horizontalInput > maxInputValue) horizontalInput = maxInputValue;
+        if (verticalInput > maxInputValue) verticalInput = maxInputValue;
+
+        //Move
+        Vector3 translation = verticalInput * Camera.transform.forward;
+        translation += horizontalInput * Camera.transform.right;
+        translation.y = 0;
+
+        if (translation.magnitude > 0) velocity = translation;
+        else velocity = Vector3.zero;
+
+        rb.AddForce(velocity * Speed);
+        rb.AddForce(0, gravityForce, 0);
+    }
+}
